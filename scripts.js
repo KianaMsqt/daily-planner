3883f7be6bfc10a05964f43520af4b28df1fb7b9
KianaMsqt <masqati.kiana@gmail.com>
2023-02-24 15:49:37 +0000

    Add class to time blocks based on current time slot

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-document.getElementById( "currentDay" ).innerHTML = moment().format('dddd, MMM Do');
+// Show Current day and date on jumbotrun
+$("#currentDay").html(moment().format('dddd, MMM Do'));
+
+$(document).ready(function() {
+    // Get the current hour in 24-hour format
+    var currentHour = moment().hour();
+  
+    // Loop through each timeblock
+    $(".time-block").each(function() {
+      // Get the hour for this timeblock
+      var blockHour = parseInt($(this).attr("data-hour"));
+  
+      // Add the appropriate class based on the current time
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  });
+  
